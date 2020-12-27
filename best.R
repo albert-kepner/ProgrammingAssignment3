83f0b8e21c51f5008734d477287d79c48534c12d
Al Kepner <albert.kepner@outlook.com>
2020-12-27 16:14:21 -0500

    best.R rc 1

--- best.R
+++ best.R
@@ -2,6 +2,11 @@
 
 best <- function(state, outcome=NA) {
   ## Read outcome data
+  
+  ## Example:
+  state <- "PA"
+  outcome <- "pneumonia"
+  
   data1 <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
   
   data2 <- data1[data1$State == state, ]
@@ -9,6 +14,7 @@ best <- function(state, outcome=NA) {
   state_rows <- length(data2$State)
   
   if(state_rows == 0) {
+    stop("invalid state")
     return (paste("No data found for state =",state))
   }
   
@@ -19,24 +25,27 @@ best <- function(state, outcome=NA) {
   } else if (outcome == "pneumonia") {
     col <- 23
   } else {
+    stop("invalid outcome")
     return (paste("No data found for outcome = ",outcome))
   }
   
-  return (names(data2)[col])
+  outcomes <- data2[ , c(2, col) ]
   
-  outcomes <- data2[ , ]
+  outcomes[,2] <- as.numeric(outcomes[,2])
   
-  matchString <- "Hospital.30.Day.Death"
-  
-  matchLen <- nchar(matchString)
-  
-  str(names(outcomes))
-
-  
+  good_data <- outcomes[ !is.na(outcomes[,2]), ]
   
   ## Return hospital name in that state with lowest 30-day death
   ## rate  
   
+
+  best_rate <- min(good_data[ ,2])
+  
+  best_rate_hospitals <- good_data[ good_data[,2] == best_rate,  ]
+  
+  first_name <- min(best_rate_hospitals[,1])
+  
+  first_name
   
 }
 
